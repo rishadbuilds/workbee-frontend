d87c4e2e1c9aeb754f94df2d54d3b70af3a206e0
rishadsinu <rishadsinu2233@gmail.com>
2026-09-22 12:58:28 +0530

    chores: cleanup backend codes

--- src/components/worker/profile/types/types.ts
+++ src/components/worker/profile/types/types.ts
@@ -1,25 +1,3 @@
-
-// export interface WorkerProfileData {
-//   _id: string;
-//   name: string;
-//   email: string;
-//   phone: string;
-//   location: string;
-//   workType: string;
-//   bio?:string;
-//   preferredWorks: string[];
-//   workerProfileImage?: string;
-//   workerProfileImagePublicId?: string;
-//   createdAt: string;
-// }
-
-// // modal props
-
-// export interface ChangePasswordModalProps {
-// isOpen: boolean;
-// setIsOpen: (isOpen: boolean) => void;
-// }
-
 export interface WorkerAddress {
   state: string;
   pincode: string;
