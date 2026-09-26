4dc4e1a553f46f5b8489f08ea07e095eba677dc6
rishadsinu <rishadsinu2233@gmail.com>
2026-09-26 15:49:32 +0530

    fix(socket): use default socket.io transport

--- src/socket/shared-connection/BaseSocketConnection.ts
+++ src/socket/shared-connection/BaseSocketConnection.ts
@@ -39,7 +39,7 @@ export abstract class BaseSocketConnection {
     this.socket = io(this.getUrl(), {
       path: this.getPath(),
       auth: { token },
-      transports: ['websocket', 'polling'],
+      // transports: ['websocket', 'polling'],
       reconnection: true,
       reconnectionAttempts: Infinity,
       reconnectionDelay: 1000,
